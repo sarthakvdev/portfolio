1094de3b02e0663b90d6b33b1ca03f6da4f6b0f6
Sarthak <55709030+sarthakvdev@users.noreply.github.com>
2020-06-26 00:48:17 +0530

    Added files of Portfolio

---
+++ js/portfolio.js
@@ -0,0 +1,7 @@
+function changeOnHover(handleName) {
+    document.querySelector('#hello').innerHTML = handleName;
+}
+
+function changeOnRemove() {
+    document.querySelector('#hello').innerHTML = "Hello";
+}
